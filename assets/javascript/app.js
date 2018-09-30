c68f805d57a817828e0740b5e2eb0abce2092467
Deezy <brad.driver33@gmail.com>
2018-09-30 15:26:58 -0400

    Initialize Firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCNNjVvV9LcD7K87ApkSz1ES3W7MQBfT6A",
+    authDomain: "train-scheduler-36fef.firebaseapp.com",
+    databaseURL: "https://train-scheduler-36fef.firebaseio.com",
+    projectId: "train-scheduler-36fef",
+    storageBucket: "train-scheduler-36fef.appspot.com",
+    messagingSenderId: "542622033065"
+  };
+  firebase.initializeApp(config);
